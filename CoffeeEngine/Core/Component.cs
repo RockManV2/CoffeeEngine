b36cccb98186f4399d52f89262aa8c15c6f45d95
Stan <sj.oosterdijk@hotmail.com>
2023-03-14 11:18:44 +0100

    Core components and utilities
    Includes:
    - abstract Component script
    - SpriteRenderer Component
    - Transform Component
    - GameObject class
    - Interfaces class
    - Static Time class
    - Static Utils class

---
+++ CoffeeEngine/Core/Component.cs
@@ -0,0 +1,11 @@
+﻿using CoffeeEngine.Physics;
+namespace CoffeeEngine;
+
+public abstract class Component
+{
+    public GameObject GameObject;
+    public Transform transform;
+    public virtual void Awake() { }
+    public virtual void Start() { }
+    public virtual void OnCollisionStay(CollisionInfo collisionInfo) { }
+}
---
+++ CoffeeEngine/Core/Utils.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+namespace CoffeeEngine;
+
+public static class Utils
+{
+    public static GraphicsDeviceManager DeviceManager;
+    public static SpriteBatch SpriteBatch;
+    public static ContentManager ContentManager;
+    public static Game Game;
+    public static void Print(object x) => Console.WriteLine(x);
+}
